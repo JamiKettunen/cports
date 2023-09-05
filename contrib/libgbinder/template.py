636adc25100c4fcb3b41f8bf5b8b001ae9119182
Jami Kettunen <jami.kettunen@protonmail.com>
2023-09-05 16:59:32 +0300

    contrib/libgbinder: new package

---
+++ contrib/libgbinder/template.py
@@ -0,0 +1,31 @@
+pkgname = "libgbinder"
+pkgver = "1.1.40"
+pkgrel = 0
+build_style = "makefile"
+make_cmd = "gmake"
+make_build_args = ["release", "pkgconfig", "KEEP_SYMBOLS=1"]
+make_install_target = "install-dev"
+make_check_target = "test"
+# TODO: ensure hardening flags etc are properly passed
+# https://github.com/chimera-linux/cports/blob/master/Packaging.md#makefile
+make_use_env = True
+hostmakedepends = ["gmake", "pkgconf"]
+makedepends = ["libglibutil-devel", "linux-headers"]
+pkgdesc = "GLib-style interface to binder, the Android IPC mechanism"
+maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
+license = "BSD-3-Clause"
+url = "https://github.com/mer-hybris/libgbinder"
+source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
+sha256 = "9e86243df6502ffd0a68ee8384c5c36b9cd4093733ea620313f1947f312abbd1"
+# FIXME: vis hides gbinder_bridge_free symbol required by gbinder-python,
+#        cfi makes unit_bridge etc SIGILL, see libglibutil patches
+hardening = ["vis", "cfi"]
+
+
+def post_install(self):
+    self.install_license("LICENSE")
+
+
+@subpackage("libgbinder-devel")
+def _(self):
+    return self.default_devel()
