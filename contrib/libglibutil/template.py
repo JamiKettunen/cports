3278d402de77ba18107fa1de2f0d30a8db75920d
Jami Kettunen <jami.kettunen@protonmail.com>
2023-09-05 10:10:59 +0300

    contrib/libglibutil: new package

---
+++ contrib/libglibutil/template.py
@@ -0,0 +1,29 @@
+pkgname = "libglibutil"
+pkgver = "1.0.79"
+pkgrel = 0
+build_style = "makefile"
+make_cmd = "gmake"
+make_build_args = ["release", "pkgconfig", "KEEP_SYMBOLS=1"]
+make_install_target = "install-dev"
+make_check_target = "test"
+# TODO: ensure hardening flags etc are properly passed
+# https://github.com/chimera-linux/cports/blob/master/Packaging.md#makefile
+make_use_env = True
+hostmakedepends = ["gmake", "pkgconf"]
+makedepends = ["glib-devel"]
+pkgdesc = "Library of glib utilities"
+maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
+license = "BSD-3-Clause"
+url = "https://github.com/sailfishos/libglibutil"
+source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
+sha256 = "4d689cb79f8ea061e46b89008370dc771b07164ee496850d9d56d9d85f1be1c3"
+hardening = ["vis", "cfi"]
+
+
+def post_install(self):
+    self.install_license("LICENSE")
+
+
+@subpackage("libglibutil-devel")
+def _(self):
+    return self.default_devel()
