0cbefb0710e5dd76e0abd748442b2cf741d4b2bb
Jami Kettunen <jami.kettunen@protonmail.com>
2024-09-26 21:27:39 +0300

    user/libhybris: allow installing mesa *gl* alongside ours

--- user/libhybris/template.py
+++ user/libhybris/template.py
@@ -98,6 +98,15 @@ def post_install(self):
     #self.mv(self.destdir / "usr/lib/libvulkan.so.*", d, glob=True)  # FIXME
     self.mv(self.destdir / "usr/lib/libOpenCL.so.*", d, glob=True)
 
+    # really not fond of this but it's the only way if we want musl to load the
+    # Android GPU drivers via libhybris by default without replacing mesa
+    # lib*gl* libs or specifying LD_LIBRARY_PATH globally while leaving
+    # software rendering just a LD_LIBRARY_PATH=/usr/lib away
+    self.mkdir(self.destdir / "etc")
+    _arch = self.profile().arch.replace("armv7", "armhf")
+    with open(self.destdir / f"etc/ld-musl-{_arch}.path", "w") as outf:
+        outf.write("/usr/local/lib:/usr/lib/hybris:/usr/lib\n")
+
     # drop junk conflicting with mesa-devel, vulkan-loader-devel & ocl-icd-devel/opencl-headers
     self.uninstall("usr/include/EGL")
     self.uninstall("usr/include/GLES")
@@ -138,7 +147,11 @@ def _gen_pkg(name, api, lib="", provides=""):
     def _(self):
         self.pkgdesc = "libhybris implementation of the {api} API"
         self.subdesc = "runtime library"
-        self.provides = [provides if provides else name]
+        # NOTE: no provides/replaces to work alongside Mesa libraries which can be used for e.g.
+        # troubleshooting via LD_LIBRARY_PATH=/usr/lib
+        self.options = ["!scanshlibs"]
+        #self.provides = [provides if provides else name]
+
         return [f"usr/lib/hybris/{lib if lib else name}.so.*"]
 
 
