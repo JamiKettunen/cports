650239d9033f1147b6078348dc0a846d64660582
Jami Kettunen <jami.kettunen@protonmail.com>
2024-10-01 12:59:36 +0300

    user/android-libhybris-users: update to 15

--- user/android-libhybris-users/template.py
+++ user/android-libhybris-users/template.py
@@ -1,11 +1,11 @@
 pkgname = "android-libhybris-users"
-pkgver = "13.0"
+pkgver = "15"
 pkgrel = 0
 depends = ["libhybris"]
-pkgdesc = "Android 13 (and below) system users for libhybris consumers"
+pkgdesc = "Android 15 (and below) system users for libhybris consumers"
 maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
 license = "Apache-2.0"
-url = f"https://android.googlesource.com/platform/system/core/+/refs/tags/android-{pkgver}.0_r1/libcutils/include/private/android_filesystem_config.h"
+url = f"https://android.googlesource.com/platform/system/core/+/refs/tags/android-{pkgver}.0.0_r1/libcutils/include/private/android_filesystem_config.h"
 
 
 def install(self):
--- user/android-libhybris-users/update.py
+++ user/android-libhybris-users/update.py
@@ -1,1 +1,2 @@
-# FIXME: broken update-check!
+url = "https://android.googlesource.com/platform/system/core/+refs"
+pattern = r">android-([\d.]+).0.0_r1<"
