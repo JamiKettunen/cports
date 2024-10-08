2793c136c96cef7894b1aaecc01a0f3d6a156e29
Jami Kettunen <jami.kettunen@protonmail.com>
2024-10-08 22:25:47 +0300

    user/halium-gsi-13.0: update to 333, add -doc

--- user/halium-gsi-13.0/template.py
+++ user/halium-gsi-13.0/template.py
@@ -1,5 +1,5 @@
 pkgname = "halium-gsi-13.0"
-pkgver = "311"
+pkgver = "333"
 pkgrel = 0
 archs = ["aarch64"]
 depends = ["lxc-android"]
@@ -7,10 +7,20 @@ pkgdesc = "Halium-patched Android 13 Generic System Image for lxc-android"
 maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
 license = "Apache-2.0"
 url = "https://ci.ubports.com/job/UBportsCommunityPortsJenkinsCI/job/ubports%25252Fporting%25252Fcommunity-ports%25252Fjenkins-ci%25252Fgeneric_arm64/job/halium-13.0"
-source = f"{url}/{pkgver}/artifact/halium_halium_arm64.tar.xz"
-sha256 = "f599b239e2eccbfce253a87b43407e772722a9a255c7df8d7bbcd900d0d9d780"
+source = [
+    f"{url}/{pkgver}/artifact/halium_halium_arm64.tar.xz>build-{pkgver}.tar.xz",
+    f"{url}/{pkgver}/artifact/halium_halium_arm64.tar.build>build-{pkgver}.txt",
+    f"{url}/{pkgver}/artifact/used-repos.txt>repos-{pkgver}.txt",
+]
+sha256 = [
+    "8ad4535a5a9bb15ced3682bd19c74b38075ecbc63005695f3ecf6d98341817ed",
+    "0f393f4ee5a09c3974bb38d73ed59bed2b2465ba399475272e7538bff4d8cf6a",
+    "87bcf3e1480fcef3c0f3e4dc7c6c2ec1445f16376e7a3806aaf2a9915fd2c627",
+]
 compression = "zstd:19"
 
 
 def install(self):
-    self.install_file("system/var/lib/lxc/android/android-rootfs.img", f"usr/lib/lxc/android")
+    self.install_file("system/var/lib/lxc/android/android-rootfs.img", "usr/lib/lxc/android")
+    self.install_file(f"build-{pkgver}.txt", f"usr/share/doc/{pkgname}", 0o644, name="build")
+    self.install_file(f"repos-{pkgver}.txt", f"usr/share/doc/{pkgname}", 0o644, name="used-repos.txt")
