d727638fd105464cd908c2a532c7be83eb2e6343
Jami Kettunen <jami.kettunen@protonmail.com>
2024-10-11 23:22:57 +0300

    [ARMv7] main/fastfetch: hack to build

--- main/fastfetch/template.py
+++ main/fastfetch/template.py
@@ -5,21 +5,22 @@ build_style = "cmake"
 configure_args = ["-DBUILD_TESTS=ON", "-DENABLE_SYSTEM_YYJSON=ON"]
 hostmakedepends = ["cmake", "ninja", "pkgconf"]
 makedepends = [
-    "chafa-devel",
+    #"chafa-devel", # armv7 FIXME harfbuzz
     "dbus-devel",
-    "dconf-devel",
-    "ddcutil-devel",
+    #"dconf-devel", # armv7 FIXME glib meson.build:2607:2: ERROR: Problem encountered: Running binaries on the build host needs to be supported to build with -Dintrospection=enabled
+    #"ddcutil-devel", # armv7 FIXME -> glib
     "elfutils-devel",
-    "libmagick-devel",
-    "libpulse-devel",
+    #"libmagick-devel", # armv7 FIXME !cross
+    #"libpulse-devel",  # armv7 FIXME -> glib
     "libxrandr-devel",
     "ocl-icd-devel",
     "vulkan-headers",
-    "vulkan-loader-devel",
+    #"vulkan-loader-devel", # armv7 FIXME !cross
     "wayland-devel",
-    "xfconf-devel",
+    #"xfconf-devel", # armv7 FIXME !cross
     "yyjson-devel",
     "zlib-ng-compat-devel",
+    "linux-headers",
 ]
 depends = ["lscpu"]
 pkgdesc = "Neofetch-like system information fetching tool"
