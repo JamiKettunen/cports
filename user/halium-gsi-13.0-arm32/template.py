29d917fff0fd362c58d7a0407349e1a2dd442cda
Jami Kettunen <jami.kettunen@protonmail.com>
2024-10-19 17:34:34 +0300

    [NOBIN] user/halium-gsi-13.0-arm32: new package
    Unbuildable for now due to no available binaries (yet)

---
+++ user/halium-gsi-13.0-arm32/template.py
@@ -0,0 +1,27 @@
+pkgname = "halium-gsi-13.0-arm32"
+pkgver = "0"
+pkgrel = 0
+archs = ["armv7", "aarch64"]
+depends = ["lxc-android"]
+pkgdesc = "Halium-patched 32-bit Android 13 Generic System Image for lxc-android"
+maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
+license = "Apache-2.0"
+# FIXME: no successful build yet
+url = "https://ci.ubports.com/job/UBportsCommunityPortsJenkinsCI/job/ubports%25252Fporting%25252Fcommunity-ports%25252Fjenkins-ci%25252Fgeneric_arm64/job/halium-13.0-arm32"
+source = [
+    f"{url}/{pkgver}/artifact/halium_halium_arm.tar.xz>build-{pkgver}.tar.xz",
+    f"{url}/{pkgver}/artifact/halium_halium_arm.tar.build>build-{pkgver}.txt",
+    f"{url}/{pkgver}/artifact/used-repos.txt>repos-{pkgver}.txt",
+]
+sha256 = [
+    "FIXME1",
+    "FIXME2",
+    "FIXME3",
+]
+compression = "zstd:19"
+
+
+def install(self):
+    self.install_file("system/var/lib/lxc/android/android-rootfs.img", "usr/lib/lxc/android")
+    self.install_file(f"build-{pkgver}.txt", f"usr/share/doc/{pkgname}", 0o644, name="build")
+    self.install_file(f"repos-{pkgver}.txt", f"usr/share/doc/{pkgname}", 0o644, name="used-repos.txt")
