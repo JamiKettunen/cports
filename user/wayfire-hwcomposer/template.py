a75bcfe1dcea2f20e8bcb4eb0d930b921b7bf61d
Jami Kettunen <jami.kettunen@protonmail.com>
2024-10-21 01:10:41 +0300

    [FIXME] user/wayfire-hwcomposer: include wvkbd for an osk
    It still doesn't appear automatically currently...

--- user/wayfire-hwcomposer/template.py
+++ user/wayfire-hwcomposer/template.py
@@ -25,7 +25,7 @@ makedepends = [
     "wf-config-devel",
     "wlroots0.17-hwcomposer-devel",
 ]
-depends = ["wlroots0.17-hwcomposer"]
+depends = ["wlroots0.17-hwcomposer", "wvkbd"]
 provides = [self.with_pkgver("wayfire")]
 replaces = ["wayfire"]
 replaces_priority = 1000
