5a86424533f80268d0d2c4b262cd4248bf0e8aff
Jami Kettunen <jami.kettunen@protonmail.com>
2024-10-21 18:02:19 +0300

    user/halium-gsi-13.0-arm32: binaries available!

--- user/halium-gsi-13.0-arm32/template.py
+++ user/halium-gsi-13.0-arm32/template.py
@@ -1,12 +1,11 @@
 pkgname = "halium-gsi-13.0-arm32"
-pkgver = "0"
+pkgver = "27"
 pkgrel = 0
 archs = ["armv7", "aarch64"]
 depends = ["lxc-android"]
 pkgdesc = "Halium-patched 32-bit Android 13 Generic System Image for lxc-android"
 maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
 license = "Apache-2.0"
-# FIXME: no successful build yet
 url = "https://ci.ubports.com/job/UBportsCommunityPortsJenkinsCI/job/ubports%25252Fporting%25252Fcommunity-ports%25252Fjenkins-ci%25252Fgeneric_arm64/job/halium-13.0-arm32"
 source = [
     f"{url}/{pkgver}/artifact/halium_halium_arm.tar.xz>build-{pkgver}.tar.xz",
@@ -14,9 +13,9 @@ source = [
     f"{url}/{pkgver}/artifact/used-repos.txt>repos-{pkgver}.txt",
 ]
 sha256 = [
-    "FIXME1",
-    "FIXME2",
-    "FIXME3",
+    "40748306cdd9fcfb964048e320da0997e709c7dfc62ece443bef9e651e6c1de9",
+    "7f731b89fe78fabf024abb78039e435c2c4d4e36894ef9675b0dfcd233554e9a",
+    "160ac9a23fb9f38a217c27bbb92dcbaa31484965b7fb79a8d924906603df1dc7",
 ]
 compression = "zstd:19"
 
