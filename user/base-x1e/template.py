493c740afbadb552c8b4019035a9294184f1cefd
Jami Kettunen <jami.kettunen@protonmail.com>
2024-11-22 03:06:25 +0200

    [DONOTSHIP] [TESTME] user/base-x1e: add per-device fw

--- user/base-x1e/template.py
+++ user/base-x1e/template.py
@@ -8,13 +8,25 @@ maintainer = "Jami Kettunen <jami.kettunen@protonmail.com>"
 license = "custom:none"
 url = "https://chimera-linux.org"
 _hwids = "abdb4b098b3958d0ca59f279e720acd8155a7fc3"
-source = f"https://github.com/anonymix007/systemd-stub/archive/{_hwids}.tar.gz"
-sha256 = "4cd2f3e5a1cd4843a1d8a17aa18b46ac0f874208c2fb20900add2a580256b286"
+_fw = "b1946a32fa504b121d4e1ad5336211e4fb3ff59f"
+source = [
+    f"https://github.com/anonymix007/systemd-stub/archive/{_hwids}.tar.gz>hwids-{_hwids}.tar.gz",
+    f"https://github.com/anonymix007/x1e-firmware/archive/{_fw}.tar.gz>fw-{_fw}.tar.gz",
+]
+sha256 = [
+    "4cd2f3e5a1cd4843a1d8a17aa18b46ac0f874208c2fb20900add2a580256b286",
+    "391cac816d8877484f99feb610d005ee1aed9c7fd14dec7abfa056be577b7f98",
+]
 options = ["empty"]
 
 
 def install(self):
-    self.install_file("json/x1e*.json", "usr/share/x1e-hwids", glob=True)
+    self.install_file(
+        f"systemd-stub-{_hwids}/json/x1e*.json",
+        "usr/share/x1e-hwids",
+        glob=True,
+    )
+    self.install_files(f"x1e-firmware-{_fw}/firmware", "usr/lib")
     # TODO: tmpfiles cp /usr/share/x1e/ukify-args /etc/kernel/ukify-args
     self.install_file(self.files_path / "ukify-args", "etc/kernel")
     self.install_file(self.files_path / "ukify-dtbs", "etc/kernel")
@@ -29,8 +41,8 @@ def _(self):
         "firmware-linux-qca",
         "firmware-linux-qcom",
     ]
-    self.options = ["empty"]
-    return []
+    self.options = ["!strip", "foreignelf", "execstack"]
+    return ["usr/lib/firmware"]
 
 
 # TODO: rename from "base-x1e-ukify" to "base-x1e-uki"?
