d221c9449df5123e037cbacbb2f38157c1f28318
Jami Kettunen <jami.kettunen@protonmail.com>
2026-01-19 13:50:57 +0200

    [FIXME] main/limine: downgrade to test broken X1E stuff
    1. long boot on old stuff
    2. without long boot it doesn't boot live ISO kernel only(?????)
    3. stubble-enabled kernel is somehow also problematic, but ONLY on
       liveiso too..

--- main/limine/template.py
+++ main/limine/template.py
@@ -1,5 +1,5 @@
 pkgname = "limine"
-pkgver = "10.5.0"
+pkgver = "10.2.1"
 pkgrel = 0
 # these targets implemented
 archs = ["aarch64", "loongarch64", "riscv64", "x86_64"]
@@ -10,7 +10,7 @@ pkgdesc = "Multiprotocol EFI bootloader"
 license = "BSD-2-Clause AND 0BSD"
 url = "https://limine-bootloader.org"
 source = f"https://codeberg.org/Limine/Limine/releases/download/v{pkgver}/limine-{pkgver}.tar.gz"
-sha256 = "da14c18eff4bda562cc44c69c7e2aebd9419ac1f1c8be32d76232eaa367503d2"
+sha256 = "b7db8d29b24a53daafc7d2c9442c8301e2de2260cc07197a9b5dd542df04e816"
 # no test suite
 options = ["!check"]
 
