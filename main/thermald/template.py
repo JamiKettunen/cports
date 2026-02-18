c92190623e4bbcb3750ffdff4c2cdf870d4c6147
Jami Kettunen <jami.kettunen@protonmail.com>
2026-02-18 14:55:30 +0200

    main/thermald: update to 2.5.11

--- main/thermald/template.py
+++ main/thermald/template.py
@@ -1,10 +1,9 @@
 pkgname = "thermald"
-pkgver = "2.5.9"
-pkgrel = 1
+pkgver = "2.5.11"
+pkgrel = 0
 archs = ["x86_64"]
 # don't use autogen.sh, it generates files that force reconf in build phase
 build_style = "gnu_configure"
-configure_args = ["--with-dbus-power-group=_thermald"]
 make_dir = "."
 hostmakedepends = [
     "autoconf-archive",
@@ -27,7 +26,7 @@ pkgdesc = "Thermal daemon for x86_64-based Intel CPUs"
 license = "GPL-2.0-or-later"
 url = "https://github.com/intel/thermal_daemon"
 source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "32fc75861e5e42b4b6ce0718af3e5b275f3febfccf6e7a3d3fbb5254791d7e74"
+sha256 = "0f4d7371d2cadf12f868e4b56d0e70af07a1c3b7d883dbe541a3707e449ea1ad"
 hardening = ["vis", "!cfi"]
 
 
@@ -43,6 +42,4 @@ def post_install(self):
         0o644,
         "org.freedesktop.thermald.service",
     )
-    self.install_license("COPYING")
     self.install_service(self.files_path / "thermald")
-    self.install_sysusers(self.files_path / "sysusers.conf")
---
+++ main/thermald/update.py
@@ -0,0 +1,2 @@
+# upstream tagging mistake with 2.5.10
+ignore = ["2.15.10"]
