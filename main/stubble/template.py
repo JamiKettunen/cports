eacc1bc23ac5295fdaf95fdd74f26ad51629c24b
Jami Kettunen <jami.kettunen@protonmail.com>
2026-05-02 15:57:59 +0300

    [SQUASHME] main/stubble: update to 9

--- main/stubble/template.py
+++ main/stubble/template.py
@@ -1,5 +1,5 @@
 pkgname = "stubble"
-pkgver = "7"
+pkgver = "9"
 pkgrel = 0
 # TODO loongarch64/riscv64: to be tested on some UEFI+FDT setup
 archs = ["aarch64", "loongarch64", "riscv64"]
@@ -11,7 +11,7 @@ pkgdesc = "Device-tree loading EFI stub"
 license = "LGPL-2.1-or-later"
 url = "https://discourse.ubuntu.com/t/spec-stubble-a-secure-boot-friendly-device-tree-loading-efi-stub"
 source = f"https://github.com/ubuntu/stubble/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "33ff21a507092328ca4495d465df31f6384fcc0dbf5a10710531713cb0cc0b17"
+sha256 = "97c5174694dcf989a7e3df69df2c1abd134fe1bbc047d7e3f274248941cb0df2"
 # No tests, busted cross-unfriendly makefile
 options = ["!check", "!cross"]
 # TODO: SOURCE_DATE_EPOCH used by elf2efi.py?
